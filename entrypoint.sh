4c4a4cbd9d37c9d6bfc5cd29a36b67c0fd6d4aec
Pedro Amador <pedroamador.rodriguez@gmail.com>
2019-05-16 18:04:24 +0200

    Update: Refactor debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-KD_NAME="hello-world"
-
 function echo_debug () {
     if [ "$KD_DEBUG" == "1" ]; then
         echo >&2 ">>>> DEBUG >>>>> $(date "+%Y-%m-%d %H:%M:%S") $KD_NAME: $@"
     fi
 }
 
-echo_debug "Hello world command"
+KD_NAME="hello-world"
+echo_debug "begin"
 
 echo "Hello World!"
+
+echo_debug "end"
