c7a0dc3c427b4f4b1c63beeb513abda68302cbf8
Pedro Amador <pedroamador.rodriguez@gmail.com>
2019-05-16 17:58:59 +0200

    Update: Add debug option and command name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+KD_NAME="hello-world"
+
+function echo_debug () {
+    if [ "$KD_DEBUG" == "1" ]; then
+        echo >&2 ">>>> DEBUG >>>>> $(date "+%Y-%m-%d %H:%M:%S") $KD_NAME: $@"
+    fi
+}
+
+echo_debug "Hello world command"
+
 echo "Hello World!"
